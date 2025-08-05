24b120bb96f2ef8e9b9772aff0cae7bc19bbf7b7
sourabhchalke <sourabhchalke2157@gmail.com>
2025-08-05 13:42:29 +0530

    Passing props from one component to another

--- basic_app/src/app/page.js
+++ basic_app/src/app/page.js
@@ -1,22 +1,28 @@
-import Image from "next/image";
+
 import styles from "./page.module.css";
 
 export default function Home() {
 
- const NewComp=()=>{
+  return (
+    <div className={styles.page}>
+      <h2>Component</h2>
+      <NewComp name={"Sachin"}/>
+      <NewComp name={"Virat"}/>
+      <NewComp name={"Dhoni"}/>
+      <NewComp name={"Rohit"}/>
+      <NewComp name={"Risahb"}/>
+      <NewComp name={"Jadeja"}/>
+    </div>
+  );
+}
+
+export const NewComp=(props)=>{
 
   return(
     <>
       <h2>NewComp</h2>
+      <h3>My Name is {props.name}</h3>
     </>
   )
 
   }
-
-  return (
-    <div className={styles.page}>
-      <h2>Component</h2>
-      <NewComp/>
-    </div>
-  );
-}
