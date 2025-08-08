b9dfc7074ad04a2e1accfb221ae505f1ccec9523
sourabhchalke <sourabhchalke2157@gmail.com>
2025-08-08 21:13:26 +0530

    Conditional Layout for aboutChildren

--- basic_app/src/app/about/layout.js
+++ basic_app/src/app/about/layout.js
@@ -1,19 +1,27 @@
+"use client";
 import Link from "next/link";
 import './about.css';
+import { usePathname } from "next/navigation";
 
 const AboutLayout=({children})=>{
+    const pathName=usePathname();
+    console.log(pathName);
     return(
         <div>
             <h2>Comman Layout for About</h2>
             <hr/>
-            <ul className="ul-menu">
+           {
+            pathName!=="/about/aboutChildren"?
+             <ul className="ul-menu">
                 <li>
                     <Link href="/about/aboutTeacher">About Teacher</Link>
                 </li>
                 <li>
                     <Link href="/about/aboutChildren">About Children</Link>
                 </li>
-            </ul>
+            </ul>:
+            null
+           }
             <h2>{children}</h2>
         </div>
     )
