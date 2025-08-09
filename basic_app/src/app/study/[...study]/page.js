fb0f7827e1bb8002ab9c9e9edb7f89e51ed86b05
sourabhchalke <sourabhchalke2157@gmail.com>
2025-08-09 18:52:47 +0530

    Catch-All segments in routing example

---
+++ basic_app/src/app/study/[...study]/page.js
@@ -0,0 +1,8 @@
+const NestedStudyPages=()=>{
+    return(
+        <div>
+            <h3>Nested Study Pages</h3>
+        </div>
+    )
+}
+export default NestedStudyPages;
---
+++ basic_app/src/app/study/page.js
@@ -0,0 +1,8 @@
+ const Study=()=>{
+    return(
+        <div>
+            <h2>Study</h2>
+        </div>
+    )
+}
+export default Study;
