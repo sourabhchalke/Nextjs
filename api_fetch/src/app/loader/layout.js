6d295b10cf5b95a140230e6f99c052c032670529
sourabhchalke <sourabhchalke2157@gmail.com>
2025-08-16 21:28:29 +0530

    Static Assests

---
+++ api_fetch/src/app/loader/layout.js
@@ -0,0 +1,10 @@
+
+import Style from "../../../public/style.css";
+
+export default function LoaderLayout({children}){
+    return(
+           <div>
+            {children}
+           </div>
+    )
+}
